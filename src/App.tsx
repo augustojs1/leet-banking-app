367f1f334091a2d84a50b86acb4ec33ea864423a
augustojs1 <augustojsouza1@gmail.com>
2022-04-05 09:24:35 -0300

    feat: setup global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
 function App() {
-  return <h1>hello world!</h1>;
+  return (
+    <div>
+      <h1>hello world!</h1>
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  background: "#f0f2f5",
+  red: "#e52e4d",
+  blue: "#5429cc",
+  lightBlue: "#6933ff",
+  titleText: "#363f5f",
+  bodyText: "#969cb3",
+  white: "#ffff",
+};
