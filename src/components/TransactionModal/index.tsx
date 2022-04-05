7f5a32cb6c01ccfe818f5dc21919eb2f94d8c4f9
augustojs1 <augustojsouza1@gmail.com>
2022-04-05 15:08:50 -0300

    fix: transaction form submit handler

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -3,8 +3,14 @@ import * as style from "./styles";
 import closeIcon from "../../assets/close.svg";
 import incomeIcon from "../../assets/income.svg";
 import expenseIcon from "../../assets/outcome.svg";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
+interface ICreateTransaction {
+  title: string;
+  type: string;
+  ammount: number;
+  category: string;
+}
 interface TransactionModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -13,12 +19,25 @@ interface TransactionModalProps {
 export function TransactionModal({ isOpen, onClose }: TransactionModalProps) {
   const [transactionType, setTransactionType] = useState("income");
 
+  const [title, setTitle] = useState("");
+  const [ammount, setAmmount] = useState(0);
+  const [category, setCategory] = useState("");
+
   function handleTypeButton(transactionType: string) {
     setTransactionType(transactionType);
   }
 
-  function handleCreateTransaction() {
-    //
+  function handleCreateTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const transactionData: ICreateTransaction = {
+      ammount,
+      category,
+      title,
+      type: transactionType,
+    };
+
+    console.log("transaction", transactionData);
   }
 
   return (
@@ -32,10 +51,20 @@ export function TransactionModal({ isOpen, onClose }: TransactionModalProps) {
         <img src={closeIcon} alt="Close transaction modal" />
       </button>
 
-      <style.form>
+      <style.form onSubmit={handleCreateTransaction}>
         <h2>Register a new transaction</h2>
-        <input type="text" placeholder="Title" />
-        <input type="number" placeholder="Ammount" />
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Ammount"
+          value={ammount}
+          onChange={(event) => setAmmount(Number(event.target.value))}
+        />
         <style.transactionType>
           <style.transationTypeButton
             type="button"
@@ -54,7 +83,12 @@ export function TransactionModal({ isOpen, onClose }: TransactionModalProps) {
             <span>Expense</span>
           </style.transationTypeButton>
         </style.transactionType>
-        <input type="text" placeholder="Category" />
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
         <button type="submit">Register</button>
       </style.form>
     </Modal>
